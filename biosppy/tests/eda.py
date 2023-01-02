54daa86213fa89aed393d628e33629e586b38f2d
PatriciaBota <36226035+PatriciaBota@users.noreply.github.com>
2023-01-02 19:55:30 +0000

    minor fixes

--- biosppy/tests/eda.py
+++ biosppy/tests/eda.py
@@ -17,36 +17,36 @@ from ..signals.eda import eda_features, eda_param
 
 
 def test():
-    eda_s = np.loadtxt("examples/eda.txt")  
+    signal = np.loadtxt("examples/eda.txt")  
     
-    on, pks, amp, end = eda_param(eda_s, filt=True, size=1.5, sampling_rate=1000)
-    for i in range(len(on)):
+    onsets, peaks, amplitudes, end = eda_param(signal, filt=True, size=1.5, sampling_rate=1000)
+    for i in range(len(onsets)):
         plt.figure()
-        plt.plot(eda_s)
-        plt.vlines(on[i], eda_s.min(), eda_s.max(), label="on", color="r")
-        plt.vlines(pks[i], eda_s.min(), eda_s.max(), label="peaks", color="b")
-        plt.vlines(end[i], eda_s.min(), eda_s.max(), label="end", color="c")
-        plt.hlines(eda_s[int(on[i])]+amp[i], 0, len(eda_s), label="amplitudes", color="k")
+        plt.plot(signal)
+        plt.vlines(onsets[i], signal.min(), signal.max(), label="on", color="r")
+        plt.vlines(peaks[i], signal.min(), signal.max(), label="peaks", color="b")
+        plt.vlines(end[i], signal.min(), signal.max(), label="end", color="c")
+        plt.hlines(eda_s[int(onsets[i])]+amplitudes[i], 0, len(signal), label="amplitudes", color="k")
         plt.legend()
         plt.show()
 
-    args = eda_features(eda_s, filt=True, size=1.5, sampling_rate=1000)
+    args = eda_features(signal, filt=True, size=1.5, sampling_rate=1000)
 
-    for i in range(len(on)):
+    for i in range(len(onsets)):
         plt.figure()
-        plt.plot(eda_s)
-        plt.vlines(args["onsets"][i], eda_s.min(), eda_s.max(), label="on", color="r")
-        plt.vlines(args["peaks"][i], eda_s.min(), eda_s.max(), label="peaks", color="b")
-        plt.hlines(eda_s[int(args["onsets"][i])]+args["amps"][i], 0, len(eda_s), label="amplitudes", color="k")
+        plt.plot(signal)
+        plt.vlines(args["onsets"][i], signal.min(), signal.max(), label="on", color="r")
+        plt.vlines(args["peaks"][i], signal.min(), signal.max(), label="peaks", color="b")
+        plt.hlines(eda_s[int(args["onsets"][i])]+args["amps"][i], 0, len(signal), label="amplitudes", color="k")
         
-        plt.vlines(args["onsets"][i] + args["rise_ts"][i], eda_s.min(), eda_s.max(), label="rise time check", color="c")
+        plt.vlines(args["onsets"][i] + args["rise_ts"][i], signal.min(), signal.max(), label="rise time check", color="c")
         try:
-            plt.vlines(args["onsets"][i] + args["half_rec"][i], eda_s.min(), eda_s.max(), label="half recovery time", color="pink")
+            plt.vlines(args["onsets"][i] + args["half_rec"][i], signal.min(), signal.max(), label="half recovery time", color="pink")
         except Exception as e:
             print(e)
             continue
         try:
-            plt.vlines(args["onsets"][i] + args["six_rec"][i], eda_s.min(), eda_s.max(), label="six recovery time", color="orange")
+            plt.vlines(args["onsets"][i] + args["six_rec"][i], signal.min(), signal.max(), label="six recovery time", color="orange")
         except Exception as e:
             print(e)
             continue
