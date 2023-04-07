dfcb198f84a7a80c374b9f11da1460021bd37f8c
Rafael Silva <rafael.c.silva@tecnico.ulisboa.pt>
2023-04-07 21:03:31 +0100

    Update time_freq.py with new structure
    time_freq_feats was renamed to time_freq, with new parameters. get_DWT was renamed to compute_wavelet. The features are now computed using the signals.tools.signal_stats function. The documentation was updated.

--- biosppy/features/time_freq.py
+++ biosppy/features/time_freq.py
@@ -3,9 +3,10 @@
 biosppy.features.time_freq
 --------------------------
 
-This module provides methods to extract time frequency features.
+This module provides methods to extract time-frequency features using
+discrete wavelet decomposition.
 
-:copyright: (c) 2015-2018 by Instituto de Telecomunicacoes
+:copyright: (c) 2015-2023 by Instituto de Telecomunicacoes
 :license: BSD 3-clause, see LICENSE for more details.
 """
 
@@ -16,93 +17,92 @@ import pywt
 
 # local
 from .. import utils
-from . import time
+from ..signals import tools as st
 
 
-def get_DWT(signal=None, wavelet="db4", level=5):
-    """Compute the signal discrete wavelet transform coefficients.
-    
+def time_freq(signal=None, wavelet="db4", level=5):
+    """Compute statistical metrics over the signal discrete wavelet transform
+    approximation and detail coefficients.
+
     Parameters
     ----------
     signal : array
         Input signal.
-    wavelet: string
-        Type of wavelet
+    wavelet: str
+        Type of wavelet. Default is db4 (Daubechies 4).
+    level: int
+        Decomposition level. Default is 5.
+
+    Returns
+    -------
+    dwt_app_{metric} : float
+        Statistical metrics over the approximation coefficients.
+    dwt_det{level}_{metric} : float
+        Statistical metrics over the detail coefficients at the specified level.
+
+    Notes
+    -----
+    Check biosppy.signals.tools.signal_stats for the list of available metrics.
+
+    """
+
+    # check inputs
+    if signal is None:
+        raise TypeError("Please specify an input signal.")
+
+    # ensure numpy
+    signal = np.array(signal)
+
+    # initialize output
+    feats = utils.ReturnTuple((), ())
+
+    # compute wavelet
+    coeffs = compute_wavelet(signal=signal, wavelet=wavelet, level=level)
+
+    # compute stats
+    for coeff, coeff_name in zip(coeffs, coeffs.keys()):
+        signal_feats = st.signal_stats(coeff)
+        for stat_value, stat_name in zip(signal_feats, signal_feats.keys()):
+            feats = feats.append(stat_value, coeff_name + '_' + stat_name)
+
+    return feats
+
+
+def compute_wavelet(signal=None, wavelet="db4", level=5):
+    """Compute the approximation and highest detail coefficients of the signal
+    using the discrete wavelet transform.
+
+    Parameters
+    ----------
+    signal : array
+        Input signal.
+    wavelet: str
+        Type of wavelet.
     level: int
         Decomposition level
 
     Returns
     -------
-    cA : list
-        Approximation coefficient
-    cD : list
-        Detail coefficient
-
-    References
-    ----------
-    - https://pywavelets.readthedocs.io/en/latest/ref/dwt-discrete-wavelet-transform.html
-    - Ghaderyan, Peyvand, and Ataollah Abbasi. "An efficient automatic workload estimation method based on electrodermal activity using pattern classifier combinations." International Journal of Psychophysiology 110 (2016): 91-101
+    dwt_app : array
+        Approximation coefficients.
+    dwt_det{level} : array
+        Detail coefficients at the specified level.
 
     """
-    
-    cD = pywt.downcoef("d", signal, wavelet, level)
-    cA = pywt.downcoef("a", signal, wavelet, level)
 
-    args = (cA, cD)
-    names = ("cA", "cD")
-    return utils.ReturnTuple(args, names)
-
-
-def time_freq_features(signal=None, sampling_rate=1000.):
-    """Compute statistical metrics describing the signal.
-   
-    Parameters
-    ----------
-    signal : array
-        Input signal.
-    sampling_rate : int, float, optional
-        Sampling Rate (Hz).
-
-    Returns
-    -------
-    DWT_cA_{time features} : list
-        Time features over the signal discrete wavelet transform approximation coefficients.
-    DWT_cD_{time features} : list
-        Time features over the signal discrete wavelet transform detail coefficients.
-
-    References
-    ----------
-    - Ghaderyan, Peyvand, and Ataollah Abbasi. "An efficient automatic workload estimation method based on electrodermal activity using pattern classifier combinations." International Journal of Psychophysiology 110 (2016): 91-101.
-    - recurrence plot: https://github.com/bmfreis/recurrence_python
-    
-    """
-    
     # check inputs
     if signal is None:
         raise TypeError("Please specify an input signal.")
-    
+
     # ensure numpy
     signal = np.array(signal)
-    args, names = [], []
 
-    ## wavelets
-    cA, cD = get_DWT(signal)
-
-    # temporal
-    _fts = time.time_features(cA, sampling_rate=sampling_rate)
-    fts_name = [str("DWT_cA_" + i) for i in _fts.keys()]
-    fts = list(_fts[:])
-
-    args += fts
-    names += fts_name
-
-    # temporal
-    _fts = time.time_features(cD, sampling_rate=sampling_rate)
-    fts_name = [str("DWT_cD_" + i) for i in _fts.keys()]
-    fts = list(_fts[:])
+    # wavelets
+    dwt_det = pywt.downcoef("d", signal, wavelet, level)
+    dwt_app = pywt.downcoef("a", signal, wavelet, level)
 
     # output
-    args = tuple(fts)
-    names = tuple(fts_name)
+    args = (dwt_app, dwt_det)
+    names = ("dwt_app", f"dwt_det{level}")
 
     return utils.ReturnTuple(args, names)
