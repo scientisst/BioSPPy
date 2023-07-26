58e5a15022721b8d102c9bcf1aa4c962dfd2c9e7
sofia3ms <sofia3ms@gmail.com>
2023-07-26 11:55:40 +0100

    Adjust formatting and documentation

--- biosppy/signals/eda.py
+++ biosppy/signals/eda.py
@@ -45,6 +45,10 @@ def eda(signal=None, sampling_rate=1000., path=None, show=True):
         Signal time axis reference (seconds).
     filtered : array
         Filtered EDA signal.
+    edr : array
+        Electrodermal response (EDR) signal.
+    edl : array
+        Electrodermal level (EDL) signal.
     onsets : array
         Indices of SCR pulse onsets.
     peaks : array
@@ -386,32 +390,32 @@ def cvx_decomposition(signal=None, sampling_rate=1000.0, tau0=2., tau1=0.7,
     delta = 1. / sampling_rate  # sampling interval in seconds
 
     # bateman ARMA model
-    a1 = 1./min(tau1, tau0) # a1 > a0
-    a0 = 1./max(tau1, tau0)
-    ar = np.array([(a1*delta + 2.) * (a0*delta + 2.), 2.*a1*a0*delta**2 - 8.,
-        (a1*delta - 2.) * (a0*delta - 2.)]) / ((a1 - a0) * delta**2)
+    a1 = 1. / min(tau1, tau0)  # a1 > a0
+    a0 = 1. / max(tau1, tau0)
+    ar = np.array([(a1 * delta + 2.) * (a0 * delta + 2.), 2. * a1 * a0 * delta ** 2 - 8.,
+                   (a1 * delta - 2.) * (a0 * delta - 2.)]) / ((a1 - a0) * delta ** 2)
     ma = np.array([1., 2., 1.])
 
     # matrices for ARMA model
     i = np.arange(2, n)
-    A = cv.spmatrix(np.tile(ar, (n-2,1)), np.c_[i,i,i], np.c_[i,i-1,i-2], (n,n))
-    M = cv.spmatrix(np.tile(ma, (n-2,1)), np.c_[i,i,i], np.c_[i,i-1,i-2], (n,n))
+    A = cv.spmatrix(np.tile(ar, (n - 2, 1)), np.c_[i, i, i], np.c_[i, i - 1, i - 2], (n, n))
+    M = cv.spmatrix(np.tile(ma, (n - 2, 1)), np.c_[i, i, i], np.c_[i, i - 1, i - 2], (n, n))
 
     # spline
     delta_knot_s = int(round(delta_knot / delta))
-    spl = np.r_[np.arange(1.,delta_knot_s), np.arange(delta_knot_s, 0., -1.)] # order 1
+    spl = np.r_[np.arange(1., delta_knot_s), np.arange(delta_knot_s, 0., -1.)]  # order 1
     spl = np.convolve(spl, spl, 'full')
     spl /= max(spl)
     # matrix of spline regressors
-    i = np.c_[np.arange(-(len(spl)//2), (len(spl)+1)//2)] + np.r_[np.arange(0, n, delta_knot_s)]
+    i = np.c_[np.arange(-(len(spl) // 2), (len(spl) + 1) // 2)] + np.r_[np.arange(0, n, delta_knot_s)]
     nB = i.shape[1]
-    j = np.tile(np.arange(nB), (len(spl),1))
-    p = np.tile(spl, (nB,1)).T
+    j = np.tile(np.arange(nB), (len(spl), 1))
+    p = np.tile(spl, (nB, 1)).T
     valid = (i >= 0) & (i < n)
     B = cv.spmatrix(p[valid], i[valid], j[valid])
 
     # trend
-    C = cv.matrix(np.c_[np.ones(n), np.arange(1., n+1.)/n])
+    C = cv.matrix(np.c_[np.ones(n), np.arange(1., n + 1.) / n])
     nC = C.size[1]
 
     # Solve the problem:
@@ -423,29 +427,29 @@ def cvx_decomposition(signal=None, sampling_rate=1000.0, tau0=2., tau1=0.7,
     cv.solvers.options.update(options)
     if solver == 'conelp':
         # Use conelp
-        z = lambda m,n: cv.spmatrix([],[],[],(m,n))
-        G = cv.sparse([[-A,z(2,n),M,z(nB+2,n)],[z(n+2,nC),C,z(nB+2,nC)],
-                    [z(n,1),-1,1,z(n+nB+2,1)],[z(2*n+2,1),-1,1,z(nB,1)],
-                    [z(n+2,nB),B,z(2,nB),cv.spmatrix(1.0, range(nB), range(nB))]])
-        h = cv.matrix([z(n,1),.5,.5,y,.5,.5,z(nB,1)])
-        c = cv.matrix([(cv.matrix(alpha, (1,n)) * A).T,z(nC,1),1,gamma,z(nB,1)])
-        res = cv.solvers.conelp(c, G, h, dims={'l':n,'q':[n+2,nB+2],'s':[]})
+        z = lambda m, n: cv.spmatrix([], [], [], (m, n))
+        G = cv.sparse([[-A, z(2, n), M, z(nB + 2, n)], [z(n + 2, nC), C, z(nB + 2, nC)],
+                       [z(n, 1), -1, 1, z(n + nB + 2, 1)], [z(2 * n + 2, 1), -1, 1, z(nB, 1)],
+                       [z(n + 2, nB), B, z(2, nB), cv.spmatrix(1.0, range(nB), range(nB))]])
+        h = cv.matrix([z(n, 1), .5, .5, y, .5, .5, z(nB, 1)])
+        c = cv.matrix([(cv.matrix(alpha, (1, n)) * A).T, z(nC, 1), 1, gamma, z(nB, 1)])
+        res = cv.solvers.conelp(c, G, h, dims={'l': n, 'q': [n + 2, nB + 2], 's': []})
         obj = res['primal objective']
     else:
         # Use qp
         Mt, Ct, Bt = M.T, C.T, B.T
-        H = cv.sparse([[Mt*M, Ct*M, Bt*M], [Mt*C, Ct*C, Bt*C],
-                    [Mt*B, Ct*B, Bt*B+gamma*cv.spmatrix(1.0, range(nB), range(nB))]])
-        f = cv.matrix([(cv.matrix(alpha, (1,n)) * A).T - Mt*y,  -(Ct*y), -(Bt*y)])
-        res = cv.solvers.qp(H, f, cv.spmatrix(-A.V, A.I, A.J, (n,len(f))),
-                            cv.matrix(0., (n,1)), solver=solver)
+        H = cv.sparse([[Mt * M, Ct * M, Bt * M], [Mt * C, Ct * C, Bt * C],
+                       [Mt * B, Ct * B, Bt * B + gamma * cv.spmatrix(1.0, range(nB), range(nB))]])
+        f = cv.matrix([(cv.matrix(alpha, (1, n)) * A).T - Mt * y, -(Ct * y), -(Bt * y)])
+        res = cv.solvers.qp(H, f, cv.spmatrix(-A.V, A.I, A.J, (n, len(f))),
+                            cv.matrix(0., (n, 1)), solver=solver)
         obj = res['primal objective'] + .5 * (y.T * y)
     cv.solvers.options.clear()
     cv.solvers.options.update(old_options)
 
     l = res['x'][-nB:]
-    d = res['x'][n:n+nC]
-    t = B*l + C*d
+    d = res['x'][n:n + nC]
+    t = B * l + C * d
     q = res['x'][:n]
     p = A * q
     r = M * q
@@ -468,8 +472,6 @@ def basic_scr(signal=None):
     ----------
     signal : array
         Input filtered EDA signal.
-    sampling_rate : int, float, optional
-        Sampling frequency (Hz).
 
     Returns
     -------
@@ -635,11 +637,11 @@ def emotiphai_eda(signal=None, sampling_rate=1000., min_amplitude=0.1,
         try:
             if sampling_rate > 1:
                 signal, _, _ = st.filter_signal(signal=signal,
-                                         ftype='butter',
-                                         band='lowpass',
-                                         order=4,
-                                         frequency=2,
-                                         sampling_rate=sampling_rate)
+                                                ftype='butter',
+                                                band='lowpass',
+                                                order=4,
+                                                frequency=2,
+                                                sampling_rate=sampling_rate)
         except Exception as e:
             print(e, "Error filtering EDA")
 
