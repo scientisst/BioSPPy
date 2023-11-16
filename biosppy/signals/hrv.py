985e0f5024763f47829398e800df2c74de6c337f
rsoaresd <rafael-luis.soares-da-costa-e-silva@inria.fr>
2023-11-16 18:39:37 +0100

    Update copyright notice

--- biosppy/signals/hrv.py
+++ biosppy/signals/hrv.py
@@ -7,7 +7,7 @@ This module provides computation and visualization of Heart-Rate Variability
 metrics.
 
 
-:copyright: (c) 2015-2018 by Instituto de Telecomunicacoes
+:copyright: (c) 2015-2023 by Instituto de Telecomunicacoes
 :license: BSD 3-clause, see LICENSE for more details.
 
 """
